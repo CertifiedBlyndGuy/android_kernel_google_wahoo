ab2a753bba60a09e9b94ff5a33ad75d1857b52ca
RJ Trujillo <certifiedblyndguy@gmail.com>
2019-07-24 10:21:42 -0600

    adreno_idler: Query display state instead of state notifier
    State notifier currently causes a kernel panic when my device enters
    suspend. After spending a lot of time trying to prevent it, I've come
    to the conclusion that directly checking the display state is probably
    a much better idea anyway
    
    Signed-off-by: RJ Trujillo <certifiedblyndguy@gmail.com>

--- drivers/devfreq/adreno_idler.c
+++ drivers/devfreq/adreno_idler.c
@@ -30,7 +30,7 @@
 
 #include <linux/module.h>
 #include <linux/devfreq.h>
-#include <linux/state_notifier.h>
+#include <linux/display_state.h>
 #include <linux/msm_adreno_devfreq.h>
 
 #define ADRENO_IDLER_MAJOR_VERSION 1
@@ -65,6 +65,8 @@ static unsigned int idlecount = 0;
 int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 		 unsigned long *freq)
 {
+        bool display_on = is_display_on();
+
 	if (!adreno_idler_active)
 		return 0;
 
@@ -83,7 +85,7 @@ int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 			idlecount--;
 			return 1;
 		}
-	} else if (state_suspended) {
+	} else if (!display_on) {
 		/* GPU shouldn't be used for much while display is off, so ramp down the frequency */
 		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
 		return 1;
